d886d3325d9cfe6a953d601409d905f4e361a46a
timelessnesses <mooping3roblox@gmail.com>
2023-07-18 17:21:43 +0700

    fuck multiplayer

--- timebomb/client/__main__.py
+++ timebomb/client/__main__.py
@@ -5,7 +5,7 @@ import os
 import requests
 import random
 import datetime
-
+import string
 class MainMenu(cli.Window):
 
     dictionary = os.path.dirname(os.path.abspath(__file__)) + "/dict.txt"
@@ -84,70 +84,7 @@ class MainMenu(cli.Window):
                         break
                 self.screen.clear()
                 self.refresh()
-                
-                health = 2
-                streak = 0
-                while health != 0:
-                    self.screen.clear()
-                    self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
-                    self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
-                    if streak >= 5:
-                        self.add_text(*self.centerize(self.generate_prompt(random.randint(1,3)), {"x": 50, "y": 80}))
-                    elif streak >= 10:
-                        self.add_text(*self.centerize(self.generate_prompt(random.randint(2,4)), {"x": 50, "y": 80}))
-                    elif streak >= 15:
-                        self.add_text(*self.centerize(self.generate_prompt(random.randint(3,5)), {"x": 50, "y": 80}))
-                    else:
-                        self.add_text(*self.centerize(self.generate_prompt(random.randint(1,2)), {"x": 50, "y": 80}))
-                    self.add_text(*self.centerize(f"Answer: ", {"x": 50, "y": 90}))        
-                    self.refresh()
-                    now = datetime.datetime.now()
-                    answer = ""
-                    used = []
-                    while True:
-                        if (datetime.datetime.now() - now).total_seconds() >= 10:
-                            health -= 1
-                            streak = 0
-                            break
-                        key = self.screen.getch()
-                        if key == curses.KEY_ENTER or key == 10:
-                            # validate answer
-                            if self.prompt in answer and answer not in used:
-                                self.add_text(*self.centerize("Congratulations!",{"x":50, "y": 70}))
-                                time.sleep(0.5)
-                                self.screen.clear()
-                                used.append(answer)
-                                break
-                            else:
-                                answer = ""
-                                self.add_text(*self.centerize("That didn't work.",{"x":50, "y": 70}))
-                                time.sleep(0.5)
-                                self.screen.clear()
-                                self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
-                                self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
-                                self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
-                                self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
-                                continue
-                        if key == curses.KEY_BACKSPACE or key == 8:
-                            answer = answer[:-1]
-                            self.screen.clear()
-                            self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
-                            self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
-                            self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
-                            self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
-                            self.refresh()
-                            continue
-                        try:
-                            if not self.is_renderable(chr(key)):
-                                continue
-                            answer += chr(key)
-                            self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
-                        except ValueError:
-                            pass
-
-
-                
-                
+                self.singleplayer_play()
             elif key in [ord("Q"), ord("q")]:
                 self.screen.clear()
                 self.refresh()
@@ -161,6 +98,116 @@ class MainMenu(cli.Window):
         else:
             self.prompt = word[:len]
             return self.prompt
+    
+    def singleplayer_play(self):
+        health = 2
+        streak = 0
+        while health != 0:
+            self.screen.clear()
+            self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+            self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
+            if streak >= 5:
+                self.add_text(*self.centerize(self.generate_prompt(random.randint(1,3)), {"x": 50, "y": 80}))
+            elif streak >= 10:
+                self.add_text(*self.centerize(self.generate_prompt(random.randint(2,4)), {"x": 50, "y": 80}))
+            elif streak >= 15:
+                self.add_text(*self.centerize(self.generate_prompt(random.randint(3,5)), {"x": 50, "y": 80}))
+            else:
+                self.add_text(*self.centerize(self.generate_prompt(random.randint(1,2)), {"x": 50, "y": 80}))
+            self.add_text(*self.centerize(f"Answer: ", {"x": 50, "y": 90}))        
+            self.refresh()
+            now = datetime.datetime.now()
+            answer = ""
+            used = []
+            while True:
+                timeout = self.determine_time(streak)
+                if (datetime.datetime.now() - now).total_seconds() >= timeout:
+                    health -= 1
+                    streak = 0
+                    self.add_text(*self.centerize(f"You had lost a heart! {health} remaining",{"x":50, "y": 70}))
+                    time.sleep(0.3)
+                    now = datetime.datetime.now()
+                    self.screen.clear()
+                    self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
+                    self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+                    self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
+                    self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
+                    if health == 0:
+                        break
+                    continue
+                key = self.screen.getch()
+                if key == curses.KEY_ENTER or key == 10:
+                    # validate answer
+                    if self.prompt in answer and answer not in used and answer in self.dictionary_words:
+                        self.add_text(*self.centerize("Congratulations!",{"x":50, "y": 70}))
+                        time.sleep(0.5)
+                        self.screen.clear()
+                        used.append(answer)
+                        break
+                    else:
+                        answer = ""
+                        self.add_text(*self.centerize("That didn't work.",{"x":50, "y": 70}))
+                        time.sleep(0.5)
+                        self.screen.clear()
+                        self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
+                        self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+                        self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
+                        self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
+                        continue
+                if key == curses.KEY_BACKSPACE or key == 8:
+                    answer = answer[:-1]
+                    self.screen.clear()
+                    self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+                    self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
+                    self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
+                    self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
+                    self.refresh()
+                    continue
+                try:
+                    if not self.is_alphabet(chr(key)):
+                        continue
+                    answer += chr(key)
+                    self.screen.clear()
+                    self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+                    self.add_text(*self.centerize("Type the word containing these alphabets!", {"x": 50, "y": 30}))
+                    self.add_text(*self.centerize("Time left:"))
+                    self.add_text(*self.centerize(self.prompt, {"x": 50, "y": 80}))
+                    self.add_text(*self.centerize(f"Answer: {answer}", {"x": 50, "y": 90}))
+                    self.refresh()
+                except ValueError:
+                    pass
+        self.screen.clear()
+        self.add_text(*self.centerize("Single player mode", {"x": 50, "y": 0}))
+        self.add_text(*self.centerize("The game has ended.", {"x": 50, "y": 50}))
+        self.add_text(*self.centerize("Press J to join again.", {"x": 50, "y": 70}))
+        self.add_text(*self.centerize("Press Q to Get out of here.", {"x": 50, "y": 80}))
+        while True:
+            key = self.screen.getch()
+            if key in [ord("J"), ord("j")]:
+                self.screen.clear()
+                self.singleplayer_play()
+                break
+            if key in [ord("Q"), ord("q")]:
+                self.screen.clear()
+                self.run()
+                break
+        self.screen.refresh()
+
+    def is_alphabet(self, char: str) -> bool:
+        return True if char in string.ascii_letters else False
+    
+    def determine_time(self, streaks: int) -> int:
+        """
+        shouldn't exceeds 5 seconds 
+        """
+        if streaks <= random.randint(5,9):
+            return 5
+        elif streaks >= random.randint(8, 13):
+            return round(random.uniform(3.8,4.7), 2)
+        elif streaks >= random.randint(12, 20):
+            return round(random.uniform(2.6, 3.6), 2)
+        elif streaks >= random.randint(18, 30):
+            return round(random.uniform(1.5, 2.4), 2)
 
 if __name__ == "__main__":
     cli.run(MainMenu)
